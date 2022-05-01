3990bb30542c80ef4ed1fb1d7eb91336d1f9e93a
ellipsiis <danielzarco.m@outlook.com>
2022-05-01 17:12:46 -0500

    feat(camera): adding the videos of security cameras with format that required in the requirements

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ app = Flask(__name__, static_folder='static')
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'FSE_SECRET_KEY'
 
+
 class LightForm(FlaskForm):
     lightOff = SubmitField('0%')
     lightMiddle = SubmitField('50%')
     lightOn = SubmitField('100%')
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/light',  methods=['GET', 'POST'])
 def light():
@@ -25,6 +28,17 @@ def light():
         'lobby': lobbyLight,
     }
     return render_template('light.html', **context)
+
+
+@app.route('/camera', methods = ['GET','POST'])
+def camera():
+    camera1 = LightForm()
+    camera2 = LightForm()
+    contex = {
+        'camera1': camera1,
+        'camera2': camera2,
+    }
+    return render_template('camera.html', **contex)
 
 
 if __name__ == '__main__':
---
+++ static/playCamera1.js
@@ -0,0 +1,8 @@
+let myVid = document.getElementById("camera1_video");
+
+function playVideo(){
+    myVid.play();
+}
+function pauseVideo(){
+    myVid.pause();
+}
